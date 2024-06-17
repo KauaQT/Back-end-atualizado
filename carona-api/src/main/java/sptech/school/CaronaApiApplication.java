4074b7d2ddc23327707ce24000608c308bf9b084
KauaQT <kaua.teles@sptech.school>
2024-06-17 17:01:21 -0300

    Api de pagamento integrada

--- carona-api/src/main/java/sptech/school/CaronaApiApplication.java
+++ carona-api/src/main/java/sptech/school/CaronaApiApplication.java
@@ -2,12 +2,14 @@ package sptech.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
+@EnableFeignClients
 public class CaronaApiApplication {
 
 	public static void main(String[] args) {
